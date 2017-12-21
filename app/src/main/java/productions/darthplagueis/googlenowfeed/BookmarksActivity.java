8531c22678af7e95a748dac4083dd0b101b9b830
osibo00 <olegsiboglov@ac.c4q.nyc>
2017-12-21 04:37:23 -0500

    minor details

--- app/src/main/java/productions/darthplagueis/googlenowfeed/BookmarksActivity.java
+++ app/src/main/java/productions/darthplagueis/googlenowfeed/BookmarksActivity.java
@@ -2,12 +2,9 @@ package productions.darthplagueis.googlenowfeed;
 
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.app.NavUtils;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
 
 import productions.darthplagueis.googlenowfeed.fragments.BookmarksFragment;
 
